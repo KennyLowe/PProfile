63540b0fa9084496345f303e07d67c2812cd5503
Kenny Lowe <kenny.lowe@outlook.com>
2018-02-07 11:18:10 +0100

    Add files via upload

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,25 @@
 #PowerShell Profile
 #Kenny Lowe 2017-2018
-#Updated 18/01/2018
+#Updated 21/01/2018
 
+
+Import-Module Get-ChildItemColor
 Import-Module Write-ASCII
+Import-Module Oh-My-Posh
 
-#Username for Azure Login
+Set-Theme Agnoster
+
+cd "Some folder"
+
 $username = "email@address.com"
-#Store Password encrypted in a file somewhere
 $pwdTxt = Get-Content ".\ExportedPassword.txt"
 $SecurePwd = $pwdTxt | ConvertTo-SecureString
 $credObject = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $SecurePwd
-#Sub you want to login to
-$SubscriptionID = your-subscription-id
+$SubscriptionID = "your subscription"
+$TranscriptDir = [string]"C:\Users\Username\Documents\WindowsPowerShell\Transcripts\"
 
 $AzureLogin = Read-Host "Login to Azure? (Y/N)"
-while("Y","N","Yes","No" -notcontains $AzureLogin)
+While("Y","N","Yes","No" -notcontains $AzureLogin)
 {
     $AzureLogin = Read-Host "Login to Azure? (Y/N)"
 }
@@ -26,10 +31,52 @@ If ($AzureLogin -eq "Y")
 	Get-AzureRmProviderFeature -FeatureName AllowArchive -ProviderNamespace  Microsoft.Storage
 }
 
+Import-Module PSReadLine
+
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
+Set-PSReadLineOption -MaximumHistoryCount 4000
+# history substring search
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Tab completion
+Set-PSReadlineKeyHandler -Chord 'Shift+Tab' -Function Complete
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
 If($Host.UI.RawUI.WindowTitle -like "*administrator*")
 {
 	Write-ASCII "Administrator" -Fore Green
 }
+
+function prompt {
+    $origLastExitCode = $LastExitCode
+    Write-VcsStatus
+
+#    Write-Host "$env:USERNAME@" -NoNewline -ForegroundColor DarkYellow
+#    Write-Host "$env:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
+#    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+
+    $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+    if ($curPath.ToLower().StartsWith($Home.ToLower()))
+    {
+        $curPath = "~" + $curPath.SubString($Home.Length)
+    }
+
+#    Write-Host $curPath -NoNewline -ForegroundColor Yellow
+    Write-Host " : " -NoNewline -ForegroundColor Blue
+    Write-Host (Get-Date -Format G) -ForegroundColor Magenta
+    $LastExitCode = $origLastExitCode
+    Write-Host $curPath -NoNewline -ForegroundColor Yellow
+    return " >"
+}
+
+Import-Module posh-git
+
+$global:GitPromptSettings.BeforeText = '['
+$global:GitPromptSettings.AfterText  = '] '
+
 
 function Test-IsPS32 
 {
@@ -101,19 +148,6 @@ banner
 }
 sal New-Scope Start-NewScope
 
-Import-Module posh-git
-
-function global:prompt {
-#  $realLASTEXITCODE = $LASTEXITCODE
-
-#  Write-Host($pwd.ProviderPath) -nonewline
-
-#  Write-VcsStatus
-
-#  $global:LASTEXITCODE = $realLASTEXITCODE
-# return "> "
-}
-
 function Test-DNSFlush
 {
 Param(
@@ -149,190 +183,12 @@ move $NewFile $OldFile
 Echo "   ** Completed **"
 }
 
-
-#
-# useful aggregate
-#
-function count
-{
-    BEGIN { $x = 0 }
-    PROCESS { $x += 1 }
-    END { $x }
-}
-
-function product
-{
-    BEGIN { $x = 1 }
-    PROCESS { $x *= $_ }
-    END { $x }
-}
-
-function sum
-{
-    BEGIN { $x = 0 }
-    PROCESS { $x += $_ }
-    END { $x }
-}
-
-function average
-{
-    BEGIN { $max = 0; $curr = 0 }
-    PROCESS { $max += $_; $curr += 1 }
-    END { $max / $curr }
-}
-
 function Get-Time { return $(get-date | foreach { $_.ToLongTimeString() } ) }
-
-function prompt
-{
-#add global variable if it doesn't already exist
-
-if (-Not $global:LastCheck) 
-{
-	$global:LastCheck = Get-Date
-    	$global:cdrive = Get-CimInstance -query "Select Freespace,Size from win32_logicaldisk where deviceid='c:'"
-
-}
-
-
-
-#only refresh disk information once every 15 minutes
-$min = (New-TimeSpan $Global:lastCheck).TotalMinutes
-
-if ($min -ge 15) 
-{
-
-    $global:cdrive = Get-CimInstance -query "Select Freespace,Size from win32_logicaldisk where deviceid='c:'"
-    $global:LastCheck = Get-Date
-}
-
-$diskinfo = "{0:N2}" -f (($global:cdrive.freespace/1gb)/($global:cdrive.size/1gb)*100)
-
-#only the get the CIM properties we need to cut down on processing time
-$cpu = (Get-CimInstance -ClassName win32_processor -property loadpercentage).loadpercentage
-
-#get the number of running processes
-$pcount = (Get-Process).Count
-$os = Get-CimInstance -class Win32_OperatingSystem -Property LastBootUpTime,TotalVisibleMemorySize,FreePhysicalMemory
-
-#calculate the percentage of free physical memory
-$freeMem = $os.freephysicalmemory/1mb
-
-#get uptime
-$time = $os.LastBootUpTime
-[TimeSpan]$uptime = New-TimeSpan $time $(get-date)
-
-#construct an uptime string e.g. 13d 15h 18m 38s
-$up = "$($uptime.days)d $($uptime.hours)h $($uptime.minutes)m $($uptime.seconds)s"
-
-#this is the text to appear in the status box
-$text="CPU:{0}% FreeMem:{6:n2}GB Procs:{1} Free C:{2}% {3}{4} {5}"  -f $cpu.ToString().padleft(2,"0"),$pcount,$diskinfo,([char]0x25b2),$up,(Get-Date -format G),$FreeMem
-
-#display prompt data in color based on the amount of free memory
-$pctFreeMem = $os.FreePhysicalMemory/$os.TotalVisibleMemorySize
-
-if ($pctFreeMem -ge .50) {
-    $color = "green"
-}
-
-elseif ($pctFreeMem -ge .20) {
-    $color = "yellow"
-}
-
-else {
-    $color = "red"
-}
-
-#write the status box with an appropriate outline color
-Write-Host $([char]0x250c) -NoNewline -ForegroundColor $color
-Write-Host $(([char]0x2500).ToString()*$text.length ) -ForegroundColor $color -NoNewline
-Write-Host $([char]0x2510) -ForegroundColor $color
-Write-Host $([char]0x2502) -ForegroundColor $color -NoNewline
-Write-Host $text -NoNewline
-Write-Host $([char]0x2502) -ForegroundColor $color
-Write-Host $([char]0x2514) -ForegroundColor $color -NoNewline
-Write-Host $(([char]0x2500).ToString()*$text.length) -NoNewline -ForegroundColor $color
-Write-Host $([char]0x2518) -ForegroundColor $color
-
-
-
-#write a prompt to the host
-    write-host "[" -noNewLine
-    write-host $(Get-Time) -foreground yellow -noNewLine
-    write-host "] " -noNewLine
-    # Write the path
-    write-host $($(Get-Location).Path | Split-Path -Leaf) -foreground green -noNewLine
-   write-host $(if ($nestedpromptlevel -ge 1) { '>>' }) -noNewLine
-
-return ""
-}
-
-function LL
-{
-    param ($dir = ".", $all = $false) 
-
-    $origFg = $host.ui.rawui.foregroundColor 
-    if ( $all ) { $toList = ls -force $dir }
-    else { $toList = ls $dir }
-
-    foreach ($Item in $toList)  
-    { 
-        Switch ($Item.Extension)  
-        { 
-            ".exe" {$host.ui.rawui.foregroundColor = "Yellow"} 
-            ".cmd" {$host.ui.rawui.foregroundColor = "Red"} 
-            ".msh" {$host.ui.rawui.foregroundColor = "Red"} 
-            ".vbs" {$host.ui.rawui.foregroundColor = "Red"} 
-            Default {$host.ui.rawui.foregroundColor = $origFg} 
-        } 
-        if ($item.Mode.StartsWith("d")) {$host.ui.rawui.foregroundColor = "Green"}
-        $item 
-    }  
-    $host.ui.rawui.foregroundColor = $origFg 
-}
-
-function lla
-{
-    param ( $dir=".")
-    ll $dir $true
-}
-
-function la { ls -force }
-
-# behave like a grep command
-# but work on objects, used
-# to be still be allowed to use grep
-filter match( $reg )
-{
-    if ($_.tostring() -match $reg)
-        { $_ }
-}
-
-# behave like a grep -v command
-# but work on objects
-filter exclude( $reg )
-{
-    if (-not ($_.tostring() -match $reg))
-        { $_ }
-}
-
-# behave like match but use only -like
-filter like( $glob )
-{
-    if ($_.toString() -like $glob)
-        { $_ }
-}
-
-filter unlike( $glob )
-{
-    if (-not ($_.tostring() -like $glob))
-        { $_ }
-}
 
 
 write-ascii "Transcript logging..." -Fore Green
 write-host ""
-$Transcript = Start-Transcript -OutputDirectory "C:\Users\kenny\OneDrive\Documents\WindowsPowerShell\Transcripts\"
+$Transcript = Start-Transcript -OutputDirectory $TranscriptDir
 write-host $Transcript -ForegroundColor "Green"
 write-host ""
 
@@ -342,6 +198,11 @@ Function funcOpenPowerShellProfile
 }
 
 Set-Alias fop funcOpenPowerShellProfile
+
+Set-Alias ls Get-ChildItemColor -option AllScope
+Set-Alias l Get-ChildItemColorFormatWide -option AllScope
+Set-Alias dir Get-ChildItemColor -option AllScope
+Set-Alias jit Invoke-Jit
 
 function get-drivespace {
   param( [parameter(mandatory=$true)]$Computer)
@@ -369,10 +230,66 @@ function Search-TextFile {
 
 Set-Alias grep Search-TextFile
 
-
-
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+function U
+{
+    param
+    (
+        [int] $Code
+    )
+ 
+    if ((0 -le $Code) -and ($Code -le 0xFFFF))
+    {
+        return [char] $Code
+    }
+ 
+    if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+    {
+        return [char]::ConvertFromUtf32($Code)
+    }
+ 
+    throw "Invalid character code $Code"
+}
+
+function invoke-jit
+{
+Import-Module Azure-Security-Center
+
+# My RG
+
+$resourceGroup = Read-Host "Resource Group?"
+
+# VM that will be started after updating the NSG
+
+$VMName = Read-Host "VM Name?"
+
+# Get my Public IP
+
+$ip = Invoke-RestMethod http://ipinfo.io/json | Select -exp ip
+
+# Hours for access
+
+[int]$hours = 3
+
+
+# Get Sub Info and select Subscription
+
+$SubscriptionName = Read-Host "Subscription Name?"
+
+Select-AzureRmSubscription -SubscriptionName $SubscriptionName
+
+
+
+# Request Access to the VM for current IP Address for RDP for 2 hours
+
+Invoke-ASCJITAccess -ResourceGroupName $resourceGroup -VM $VMName -Port 3389 -Hours $hours -AddressPrefix $ip 
+
+}
+
+Set-Theme Agnoster
+Import-Module PoShFuck
